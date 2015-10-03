07483f8b3a915278a4d45a21c215c48ec15a04ed
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-10-03 11:24:30 +0200

    Add dummy logging server for debugging purposes on port 8000

--- repl/user.clj
+++ repl/user.clj
@@ -8,16 +8,35 @@
             [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [com.stuartsierra.component :as component]
             [buergeramt-sniper.core]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [org.httpkit.server :as server]
+            [clojure.tools.logging :as log]))
 
 (def system
   "A Var containing an object representing the application under
   development."
   nil)
 
+(defonce dev-server (atom nil))
+
+(defn dev-server-fn [req]
+  (log/spy req)
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "<html><body>YES</body></html>"})
+
+(defn stop-dev-server []
+  (when-not (nil? @dev-server)
+    (@dev-server :timeout 100)
+    (reset! dev-server nil)))
+
+(defn start-dev-server []
+  (reset! dev-server (server/run-server #'dev-server-fn {:port 8000})))
+
 (defn start
   "Starts the system running, sets the Var #'system."
   []
+  (start-dev-server)
   (alter-var-root #'system
                   (constantly (buergeramt-sniper.core/init-debug))))
 
@@ -26,7 +45,8 @@
   #'system."
   []
   (alter-var-root #'system
-                  (fn [s] (when s (component/stop s)))))
+                  (fn [s] (when s (component/stop s))))
+  (stop-dev-server))
 
 (defn go
   "Initializes and starts the system running."
